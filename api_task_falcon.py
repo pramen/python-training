9abcc929d425f28d38393f2faa38b65adeb11f0f
Alexander <alexander.mikhnovets@gmail.com>
2016-11-08 19:36:48 +0300

    little refactoring

--- api_task_falcon.py
+++ api_task_falcon.py
@@ -27,23 +27,29 @@ class PSResource:
                     'pid',
                     'terminal',
                     'cmdline',
-                    'cpu_times'
+                    'cpu_times',
                 ]
             ) for proc in psutil.process_iter() if proc.as_dict(attrs=['username'])['username'] == username
         )
 
-        user_procs_attrs_filled = [
-            {
-                'pid': p['pid'],
-                'tty': p['terminal'],
-                'cmd': p['cmdline'][0] if p['cmdline'] else None,
-                'time': str( datetime.timedelta(
-                    seconds=int(sum(p['cpu_times'][0:2]))
-                )) if p['cpu_times'] else None,
-            } for p in user_procs_attrs
-        ]
 
-        resp.body = json.dumps(user_procs_attrs_filled)
+        def format_proc_attrs(proc_attrs):
+
+            proc_attrs_result = {
+                'pid': proc_attrs['pid'],
+                'tty': proc_attrs['terminal'],
+                'cmd': proc_attrs['cmdline'][0] if proc_attrs['cmdline'] else None,
+                'time': str(datetime.timedelta(
+                    seconds=int(sum(proc_attrs['cpu_times'][0:2]))
+                )) if proc_attrs['cpu_times'] else None,
+            }
+
+            return proc_attrs_result
+
+
+        user_procs_attrs_formatted = [ format_proc_attrs(p_attrs) for p_attrs in user_procs_attrs ]
+
+        resp.body = json.dumps(user_procs_attrs_formatted)
 
 
 class CPUModelResource:
